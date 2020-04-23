0f37f190bc26686fae6a071745f399d696add0b1
budoucha <budoucha@gmail.com>
2020-04-24 03:02:58 +0900

    ver 1

---
+++ sketch.js
@@ -0,0 +1,35 @@
+let audio_enabled = false;
+
+function preload(){
+  mitsu_button_image = loadImage("assets/mitsu_button.png");
+  mitsu_sound = loadSound('assets/mitsu.mp3');
+}
+
+function setup(){
+  canvas = createCanvas(mitsu_button_image.width, mitsu_button_image.height);
+  canvas.parent('sketch-holder');
+  background(0,0);
+
+  mitsu_button = createSprite(width/2, height/2);
+  mitsu_button.addImage(mitsu_button_image);
+  mitsu_button.mouseActive = true;
+  mitsu_button.onMousePressed = () => {
+    tint(168);
+    mitsu_sound.play();
+  }
+  mitsu_button.onMouseReleased = () => noTint();
+
+  audioSetup();
+}
+
+function draw(){
+  drawSprites();
+
+}
+
+function audioSetup(){
+  audio_enabled = true;
+  masterVolume(1.0);
+  mitsu_sound.setVolume(1.0);
+  mitsu_sound.playMode('restart');
+}
